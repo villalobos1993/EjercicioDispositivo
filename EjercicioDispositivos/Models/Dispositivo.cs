f020420f021caad3765e9ab928013b4ee75e0fae
villalobo <alevillalobos.1993@gmail.com>
2017-06-07 13:47:09 +0200

    version 1

---
+++ EjercicioDispositivos/Models/Dispositivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDispositivos.Models
+{
+    abstract class Dispositivo
+    {
+
+        protected String Marca;
+        protected String Modelo;
+        protected int Pulgadas;
+        protected string SO;
+
+        private Dispositivo() { }
+
+        public Dispositivo(string marca,String modelo)
+        {
+
+            this.Marca = marca;
+            this.Modelo = modelo;
+        }
+
+        public Dispositivo(String Marca,String Modelo,int pulgadas) : this (Marca,Modelo)
+        {
+            this.Pulgadas = pulgadas;
+
+        }
+
+        
+    }
+}
